686f1ff283c742d66c1d03f1e4f17f15093883eb
Armando Berlanga <Jose.berlangam@udem.edu>
2022-10-06 00:20:30 +0200

    feat: primera implentacion de json reader

--- src/main/java/com/ufv/persona/backend/PersonaList.java
+++ src/main/java/com/ufv/persona/backend/PersonaList.java
@@ -2,8 +2,14 @@ package com.ufv.persona.backend;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PersonaList {
     List<Persona> value;
@@ -22,12 +28,11 @@ public class PersonaList {
         for(Persona p : value) json += p + ",";
         json = json.substring(0, json.length() - 1);
 
-        return "{ \"personas\": [" + json + "] }";
+        return "[" + json + "]";
     }
 
     public void createJSONFile(String fileName) {
-        String path = System.getProperty("user.dir") + "/src";
-        path += "/" + fileName + ".json";
+        String path = System.getProperty("user.dir") + "/src/" + fileName + ".json";
 
         File file = new File(path);
         if(file.exists()) file.delete();
@@ -42,6 +47,29 @@ public class PersonaList {
         } catch (Exception e) {
             System.out.println("\nError creating JSON file\n");
         }
+    }
+
+    public void readJSONFile(String fileName) {
+        Path path = Path.of(System.getProperty("user.dir") + "/src/" + fileName + ".json");
+
+        String content = null;
+        try {
+            content = Files.readString(path);
+        } catch (IOException e) {
+            System.out.println("\nError reading JSON file\n");
+        }
+
+        if(content == null) return;
+
+        Persona[] payload = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            payload = mapper.readValue(content, Persona[].class);
+        } catch (IOException e) {
+            System.out.println("\nError parsing JSON file\n");
+        }
+
+        this.value = new ArrayList<Persona>(Arrays.asList(payload));
     }
 
     @Override
